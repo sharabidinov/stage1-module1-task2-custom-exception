e40bf03faa6714d2c03a42c560218a7aaaf255e9
sharabidinov <alymbeksharabidinov1@gmail.com>
2023-11-14 19:49:55 +0600

    solution 3

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -3,19 +3,23 @@ package com.epam.mjc;
 
 public class StudentManager {
 
-  private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+    private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
-  public Student find(long studentID) {
-    return Student.getValueOf(studentID);
-  }
-
-  public static void main(String[] args) {
-    StudentManager manager = new StudentManager();
-
-    for (int i = 0; i < IDs.length; i++) {
-      Student student = manager.find(IDs[i]);
-      System.out.println("Student name " + student.getName());
+    public Student find(long studentID) throws MyException {
+        if (Student.getValueOf(studentID) != null) {
+            return Student.getValueOf(studentID);
+        } else {
+            throw new MyException("Could not find student with ID " + studentID);
+        }
     }
 
-  }
+    public static void main(String[] args) {
+        StudentManager manager = new StudentManager();
+
+        for (int i = 0; i < IDs.length; i++) {
+            Student student = manager.find(IDs[i]);
+            System.out.println("Student name " + student.getName());
+        }
+
+    }
 }
